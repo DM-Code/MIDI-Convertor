6b3ce12fb52d6e9c4a742ad8c92431d345f2e0b0
DM-Code <58431780+DM-Code@users.noreply.github.com>
2023-09-28 12:15:07 +0100

    Added indentation to some class functions that didn't have, all class functions can be called from the main now

--- main.py
+++ main.py
@@ -20,16 +20,11 @@ if __name__ == '__main__':
     start_text()
 
     midi_handler = MidiHandler('resources/bach_846.mid')
-    midi_handler.play()
 
-    # default_midi_output = pygame.midi.get_default_output_id()
-    # output = pygame.midi.Output(default_midi_output)
-    #
-    # print("Filename: {} \n".format(str(mid.filename).split('/')[1]))
-    #
-    # playable_tracks = get_playable_midi_tracks()
-    # print(playable_tracks)
-    # show_all_midi_tracks()
+
+    playable_tracks = midi_handler.get_playable_midi_tracks()
+    print(playable_tracks)
+    midi_handler.show_all_midi_tracks()
 
     # Only play audio from a certain channel
 
--- midi_handler.py
+++ midi_handler.py
@@ -1,6 +1,8 @@
 import pygame.midi
 from mido import MidiFile
 
+from main import line_space
+
 
 def stop():
     pygame.midi.quit()
@@ -55,65 +57,56 @@ class MidiHandler:
                     self.output.note_off(note)
                     print("Note Off: {}".format(note))
 
+    def show_midi_data(self):
+        note_channels = []
+        for i, track in enumerate(self.mid.tracks):
+            print('Track {}: {}'.format(i, track.name))
+            for msg in track:
+                if msg.is_meta:
+                    ...
+                if msg.type == "note_on":
+                    note_channels.append(i)
+                    print("Appended")
+                    break
 
-def show_midi_data(self):
-    note_channels = []
-    for i, track in enumerate(self.mid.tracks):
-        print('Track {}: {}'.format(i, track.name))
-        for msg in track:
-            if msg.is_meta:
-                ...
-            if msg.type == "note_on":
-                note_channels.append(i)
-                print("Appended")
-                break
+                # print(msg)
+            line_space()
 
-            # print(msg)
+    def show_all_midi_tracks(self):
+        # Track Names
+        # TODO: Have this added to a list over only one for each loop
+        for i, track in enumerate(self.mid.tracks):
+            print('Track {}: {}'.format(i, track.name))
         line_space()
 
+    def get_playable_midi_tracks(self):
+        note_channels = []
+        for i, track in enumerate(self.mid.tracks):
+            for msg in track:
+                if msg.is_meta:
+                    ...
+                if msg.type == "note_on":
+                    note_channels.append(i)
+                    break
+        return note_channels
 
-def show_all_midi_tracks(self):
-    # Track Names
-    # TODO: Have this added to a list over only one for each loop
-    for i, track in enumerate(self.mid.tracks):
-        print('Track {}: {}'.format(i, track.name))
-    line_space()
+    # Channel
+    def show_midi_data_channel(self, channel):
+        for i, track in enumerate(self.mid.tracks):
+            print('Track {}: {}'.format(i, track.name))
+            for msg in track:
+                if msg.is_meta:
+                    ...
+                print(msg)
+            line_space()
 
+        for msg in self.mid.play():
+            ...
+            # port.send(msg)
 
-def get_playable_midi_tracks(self):
-    note_channels = []
-    for i, track in enumerate(self.mid.tracks):
-        for msg in track:
-            if msg.is_meta:
-                ...
-            if msg.type == "note_on":
-                note_channels.append(i)
-                break
-    return note_channels
+    def get_channel_count(self):
+        channel_count = 0
+        for i, track in enumerate(self.mid.tracks):
+            channel_count += 1
 
-
-# Channel
-def show_midi_data_channel(self, channel):
-    for i, track in enumerate(self.mid.tracks):
-        print('Track {}: {}'.format(i, track.name))
-        for msg in track:
-            if msg.is_meta:
-                ...
-            print(msg)
-        line_space()
-
-    for msg in self.mid.play():
-        ...
-        # port.send(msg)
-
-
-def get_channel_count(self):
-    channel_count = 0
-    for i, track in enumerate(self.mid.tracks):
-        channel_count += 1
-
-    return channel_count
-
-
-def line_space():
-    print("______________________________________________")
+        return channel_count
