75c0484850b4f8982d996b41ea397a11ec9d55ff
DM-Code <58431780+DM-Code@users.noreply.github.com>
2023-09-28 11:37:24 +0100

    Plays a midi file through

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from mido import Message, MidiFile
+import mido
+import pygame.midi
+import time
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
+def line_space():
+    print("______________________________________________")
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pygame.midi.init()
+    mid = MidiFile('resources/bach_846.mid')
+
+    default_midi_output = pygame.midi.get_default_output_id()
+
+    # Selects the default output device
+    output = pygame.midi.Output(default_midi_output)
+
+    for message in mid.play():
+        if message.type == 'note_on':
+            note = message.note
+            velocity = message.velocity
+            time = message.time
+            output.note_on(note, velocity)
+
+            if(velocity != 0):
+                print("Note On: {}".format(note))
+            else:
+                print("Note Off: {}".format(note))
+
+        elif message.type == 'note_off':
+            output.note_off(note)
+            print("Note Off: {}".format(note))
+
+    # Track Names
+    # TODO: Have this added to a list over only one for each loop
+    # for i, track in enumerate(mid.tracks):
+    #     print('Track {}: {}'.format(i, track.name))
+    # line_space()
+    #
+    # for i, track in enumerate(mid.tracks):
+    #     print('Track {}: {}'.format(i, track.name))
+    #     for msg in track:
+    #         if msg.is_meta:
+    #             ...
+    #         print(msg)
+    #     line_space()
+    #
+    # for msg in mid.play():
+    #     ...
+    #     # port.send(msg)
+    #
+
+
+    pygame.midi.quit()
+
